8e4eebfaf362d6819f7e62cf5578b6b56a5410f8
Yuriy <mumrenkoyuriy@gmail.com>
2017-06-29 11:15:24 +0300

    first commit

---
+++ js/hw1.js
@@ -0,0 +1,9 @@
+
+alert (document.getElementById("one").innerHTML);
+document.getElementById('ttt').innerHTML= "Hi, dude! How are you doing?";
+var newP = document.createElement('p');
+newP.innerHTML = 'Hello world!';
+wrapperText.insertBefore(newP, wrapperText.children[1]);
+
+
+
